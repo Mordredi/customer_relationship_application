860bb2252dc56cb2ae069b961b8e634171c3fa71
Arthur <arthur.p.wright@gmail.com>
2015-04-01 16:00:22 -0400

    Started CMR application. Built method to display initial user options in cmr.rb

---
+++ crm.rb
@@ -0,0 +1,8 @@
+def print_main_menu
+  puts "[1] Add a contact"
+  puts "[2] Modify a contact"
+  puts "[3] Diplay all contacts"
+  puts "[4] Display an attribute"
+  puts "[5] Delete a contact"
+  puts "[6] Exit"
+end
